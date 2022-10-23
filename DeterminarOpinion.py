fb8fc4a6507bb8c7ad40a0bc6ab8ffe2b3e80108
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-23 19:17:08 +0200

    gg

---
+++ DeterminarOpinion.py
@@ -0,0 +1,133 @@
+import pandas as pnd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+
+
+class determina_opinion:
+    def __init__(self,dataset):
+        self.dataset=dataset
+
+    def cargar_archivo(self):
+        self.mensajesTwitter = pnd.read_csv(self.dataset, delimiter=";")
+        nltk.download('stopwords')
+        nltk.download('wordnet')
+        self.stopWords = stopwords.words('english')
+        self.stemmer = SnowballStemmer('english')
+        self.lemmatizer = WordNetLemmatizer()
+
+    def inf(self):
+        print(self.mensajesTwitter.shape)
+        print(self.mensajesTwitter.head(2))
+
+
+    def normalizacion(self):
+        for mensaje in self.mensajesTwitter["TWEET"]:
+            mensaje = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL', mensaje)
+            mensaje = re.sub('@[^\s]+','USER', mensaje)
+            mensaje = mensaje.lower().replace("ё", "е")
+            mensaje = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', mensaje)
+            mensaje = re.sub(' +',' ', mensaje)
+            mensaje.strip()
+        print(self.mensajesTwitter.head(10))
+
+    def normalizacion_frase(mensaje):
+        mensaje = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL', mensaje)
+        mensaje = re.sub('@[^\s]+','USER', mensaje)
+        mensaje = mensaje.lower().replace("ё", "е")
+        mensaje = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', mensaje)
+        mensaje = re.sub(' +',' ', mensaje)
+        return mensaje.strip()
+
+    def transformar_columna(self,columna):
+        self.mensajesTwitter[columna] = (self.mensajesTwitter[columna]=='Yes').astype(int)
+        print(self.mensajesTwitter.head(100))
+
+    def eliminar_stopwords(self):
+        self.mensajesTwitter['TWEET'] = self.mensajesTwitter['TWEET'].apply(lambda mensaje: ' '.join([palabra for palabra in mensaje.split() if palabra not in (self.stopWords)]))
+        print(self.mensajesTwitter.head(10))
+
+    def stemming(self):
+        self.mensajesTwitter['TWEET'] = self.mensajesTwitter['TWEET'].apply(lambda mensaje: ' '.join([self.stemmer.stem(palabra) for palabra in mensaje.split(' ')]))
+        print(self.mensajesTwitter.head(10))
+
+    def preparacion(self):
+        self.cargar_archivo()
+        self.inf()
+        self.transformar_columna('CREENCIA')
+        self.normalizacion()
+        self.eliminar_stopwords()
+        self.stemming()
+        print('')
+        print('################ FIN DE LA PREPARACION ################')
+        print('')
+
+    def Aprendizaje_multinomial(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+        self.etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', MultinomialNB())])
+        self.modelo = self.etapas_aprendizaje.fit(self.X_train,self.y_train)
+        print(classification_report(self.y_test, self.modelo.predict(self.X_test), digits=4))
+
+    def predecir_frase(self):
+        self.frase = "Why should trust scientists with global warming if they didnt know Pluto wasnt a planet"
+        print(self.frase)
+
+        #Normalización
+        frase = self.normalizacion_frase(frase)
+
+        #Eliminación de las stops words
+        frase = ' '.join([palabra for palabra in frase.split() if palabra not in (self.stopWords)])
+
+        #Aplicación de stemming
+        frase =  ' '.join([self.stemmer.stem(palabra) for palabra in frase.split(' ')])
+
+        #Lematización
+        frase = ' '.join([self.lemmatizer.lemmatize(palabra) for palabra in frase.split(' ')])
+        print (frase)
+
+        prediccion = self.modelo.predict([frase])
+        print(prediccion)
+        if(prediccion[0]==0):
+            print(">> No cree en el calentamiento climático...")
+        else:
+            print(">> Cree en el calentamiento climático...")
+
+    def aprendizaje_svm(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+        self.etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', svm.SVC(kernel='linear', C=2))])
+        self.modelo = self.etapas_aprendizaje.fit(self.X_train,self.y_train)
+        print(classification_report(self.y_test, self.modelo.predict(self.X_test), digits=4))
+
+    def aprendizaje_svm_avanzado(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.mensajesTwitter['TWEET'].values,  self.mensajesTwitter['CREENCIA'].values,test_size=0.2)
+        self.etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),('tfidf', TfidfTransformer()),('algoritmo', svm.SVC(kernel='linear', C=2))])
+        self.modelo = self.etapas_aprendizaje.fit(self.X_train,self.y_train)
+
+        parametrosC = {'algoritmo__C':(1,2,4,5,6,7,8,9,10,11,12)}
+
+        self.busquedaCOptimo = GridSearchCV(self.etapas_aprendizaje, parametrosC,cv=2)
+        self.busquedaCOptimo.fit(self.X_train,self.y_train)
+        print(self.busquedaCOptimo.best_params_['algoritmo__C'])
+
+        #Parámetro nuevo C=1
+        etapas_aprendizaje = Pipeline([('frequencia', CountVectorizer()),
+                                        ('tfidf', TfidfTransformer()),
+                                        ('algoritmo', svm.SVC(kernel='linear', C=self.busquedaCOptimo.best_params_['algoritmo__C']))])
+
+        modelo = etapas_aprendizaje.fit(self.X_train,self.y_train)
+        print(classification_report(self.y_test, modelo.predict(self.X_test), digits=4))
+
+
+
+op=determina_opinion('calentamientoClimatico.csv')
+op.preparacion()
