a677dbaee6dd7c42fdeb99606dc464844c93d29d
Javifdz12 <101470100+Javifdz12@users.noreply.github.com>
2022-10-24 13:41:59 +0200

    gg

--- menu.py
+++ menu.py
@@ -1,40 +1,37 @@
 import DeterminarOpinion as dt
 import helpers
-import os
+
+def menu():
+    print("========================")
+    print(" BIENVENIDO AL Manager ")
+    print("========================")
+    print("[1] Aprendizaje multinomial ")
+    print("[2] Aprendizaje SVM ")
+    print("[3] Aprendizaje SVM Avanzado ")
+    print("[4] Salir ")
+    print("========================")
 
 def iniciar():
-
-    tweets = dt.determina_opinion('datas\calentamientoClimatico.csv')
+    tweets = dt.determina_opinion('calentamientoClimatico.csv')
     tweets.preparacion()
+    helpers.limpiar_pantalla()
 
     while True:
 
-         # os.system('cls')
-        print("========================")
-        print(" BIENVENIDO AL Manager ")
-        print("========================")
-        print("[1] Aprendizaje multinomial ")
-        print("[2] Aprendizaje SVM ")
-        print("[3] Aprendizaje SVM Avanzado ")
-        print("[4] Salir ")
-        print("========================")
-
-        opcion = input("> ")
-
-        # helpers.limpiar_pantalla()
-
+        menu()
+        opcion = int(input("> "))
+        helpers.limpiar_pantalla()
 
         if opcion == 1:
+            tweets.Aprendizaje_multinomial()
             while True:
-                tweets.Aprendizaje_multinomial()
-                helpers.limpiar_pantalla()
                 print("========================")
                 print("[1] Predecir frase")
-                print("[2] No predecir frase")
+                print("[2] Volver")
                 print("========================")
 
-                opcion_1 = input("> ")
-                # helpers.limpiar_pantalla()
+                opcion_1 = int(input("> "))
+                helpers.limpiar_pantalla()
 
                 if opcion_1 == 1:
                     print('Introduce una frase(en inglés)')
@@ -53,7 +50,5 @@ def iniciar():
         if opcion == 4:
             print("Saliendo...\n")
             break
-
-
 
 
